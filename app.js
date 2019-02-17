2fc2236280cc488572848cd5e5e26f80a380a8d1
Yvee1 <stevenvdb@live.nl>
2019-02-17 18:03:44 +0100

    Setting up the code structure and entered the questions

---
+++ app.js
@@ -0,0 +1,49 @@
+let state = { q1 : {
+    question: "What is an array?";
+    correct: "A collection of objects stored in linear order accessible by a integer index.";
+    answer1: "A well defined computational procedure that takes some value and produces some value.";
+    answer2: "A sequence of computational steps that transform the input into the output.";
+    answer3: "Collection of nodes, which together represents a sequence, where each node contains data and a reference to the next node in the sequence.";
+  }, q2 : {
+    question: "What is not part of a complete description of an algorithm?";
+    answer1: "The algorithm";
+    answer2: "Proof of correctness";
+    answer3: "Derivation of running time";
+    correct: "Hardware requirements";
+  }, q3 : {
+    question: "Which of the following sorting algorithms is incremental?";
+    answer1: "Bubble sort";
+    answer2: "Merge sort";
+    correct: "Insertion sort";
+    answer3: "Quick sort";
+  }, q4 : {
+    question: "Which of these steps is not part of a proof by loop invariant?";
+    answer1: "Initialization";
+    answer2: "Termination";
+    answer3: "Maintenance";
+    correct: "Augmentation";
+  }, q5 : {
+    question: "Which of the following operations is not needed for a max-priorty queue?";
+    answer1: "Increase-Key";
+    answer2: "Maximum";
+    answer3: "Extract-Max";
+    correct: "Decrease-Key"
+  }, current-question: 0, questions-correct: 0
+
+}
+
+function hideStartScreen() {
+
+};
+
+function renderQuestion(state, index_question) {
+
+};
+
+function doAllTheThings() {
+
+};
+
+$(function(
+  doAllTheThings();
+));
